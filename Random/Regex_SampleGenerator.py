11e7aa924a9ae02ba2b6e9a0926774a5b3e77820
Shahriar-0 <attarshahriar@gmail.com>
2024-03-13 01:11:41 +0330

    fix regex generator

--- Random/Regex_SampleGenerator.py
+++ Random/Regex_SampleGenerator.py
@@ -1,15 +1,19 @@
 import re
 import random
 
-pattern = re.compile(r"(\d+)|(\+)|(\*)|(\()|(\))")
+pattern = re.compile(r"([a-z]+_)*[a-z]+")
 
 ans = ""
 matches = []
+not_matches = []
 
-while len(matches) < 10:
-    length = random.randint(1, 40)
-    ans = "".join([random.choice("0123456789+*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(length)])
-    if re.match(pattern, ans):
+while len(matches) < 10 or len(not_matches) < 10:
+    length = random.randint(10, 20)
+    ans = "".join([random.choice("0123456789+*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-") for _ in range(length)])
+    if re.fullmatch(pattern, ans):  # if re.match(pattern, ans):
         matches.append(ans)
+    else:
+        not_matches.append(ans)
 
-print(matches)
+print(f"----------matched:\n{matches[:10]}")
+print(f"----------not matched:\n{not_matches[:10]}")
